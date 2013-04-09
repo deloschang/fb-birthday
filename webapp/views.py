d0e4881dd46a7289857ca4d2e180fbcae7546d3c
Delos Chang <delosfchang@gmail.com>
2013-04-08 22:29:29 -0400

    implementing login switch

--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render_to_response
+from facepy import GraphAPI
 
 def home(request):
-    return render_to_response("main.html")
+    # test for successful login
+    try: 
+        access_token = request.user.social_auth.all().get(user=request.user, provider='facebook').extra_data['access_token']
+
+        # pull the birthday data from facebook
+        data_amount = pull_facebook(access_token)
+
+
+        return render_to_response('loggedin.html')
+    except AttributeError:
+        return render_to_response('main.html')
+
+def pull_facebook(access_token):
+    graph = GraphAPI(access_token)
+
+
