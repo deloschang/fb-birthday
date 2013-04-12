a8fe1f7eae44db9569ab2a28ce7dea9fcc194e83
Delos Chang <delosfchang@gmail.com>
2013-04-12 19:04:14 -0400

    importing datetiem, setting up for analysis

--- webapp/management/commands/search.py
+++ webapp/management/commands/search.py
@@ -1,7 +1,11 @@
 from django.core.management.base import BaseCommand, CommandError
 from webapp.models import *
 
+import datetime  
+
 class Command(BaseCommand):
     # must be named handle
     def handle(self, *args, **options):
+        # process every date in the database
+        # match date today
         self.stdout.write('Successful call')
